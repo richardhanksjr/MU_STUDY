875a68ffc122dfbc9d8aaaad4da93fee6c0f42c5
Richard Hanks <richardhanksjr@gmail.com>
2016-12-04 21:42:23 -0500

    Add questions app and set up initial routing

--- mu_study/questions/views.py
+++ mu_study/questions/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+def nextquestion(request):
+    context = {'question': 'question'}
+    template = 'questions/test.html'
+    return render(request, template, context)
